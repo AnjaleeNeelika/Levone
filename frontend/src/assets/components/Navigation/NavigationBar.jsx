55df402ae80e8632a08148b925063590464f583e
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-11-05 01:06:14 +0530

    Project initialization

---
+++ frontend/src/assets/components/Navigation/NavigationBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavigationBar = () => {
+    return (
+        <nav className='flex items-center py-6  px-8 justify-between'>
+            <div></div>
+        </nav>
+    )
+}
+
+export default NavigationBar
