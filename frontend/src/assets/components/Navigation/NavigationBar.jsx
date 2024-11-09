c51df7f682e73bcfdd02318e56b583b1156053ca
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-11-09 23:58:55 +0530

    Search tab and icons on navigation bar

--- frontend/src/assets/components/Navigation/NavigationBar.jsx
+++ frontend/src/assets/components/Navigation/NavigationBar.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Logo from '../../logo/transparent_bg.png'
+import { IoSearchOutline } from 'react-icons/io5'
 
 const NavigationBar = () => {
     return (
         <nav className='flex items-center py-6 px-16 justify-between gap-40'>
             <div className='flex items-center gap-6'>
                 {/* Logo */}
-                <a href='/' className='text-3xl text-black gap-8'>Levone</a>
+                <a href='/' className='text-3xl text-black gap-8 font-bold'>Levone</a>
                 {/* <img src={Logo} className='h-14' /> */}
             </div>  
             <div className='flex flex-wrap items-center gap-10 flex-1'>
@@ -19,12 +20,23 @@ const NavigationBar = () => {
                 </ul>
             </div>          
 
-            <div>
+            <div className='flex justify-center'>
                 {/* Search Bar */}
+                <div className='border rounded flex overflow-hidden'>
+                    <div className='flex items-center justify-center px-4 order-1 bg-gray-100'>
+                        <IoSearchOutline />
+                        <input type="text" name="" id="" className='px-4 py-2 outline-none bg-transparent' placeholder='Search' />
+                    </div>
+                </div>
             </div>
 
-            <div>
+            <div className='flex flex-wrap items-center gap-4'>
                 {/* Action items - icons */}
+                <ul className='flex items-center gap-8'>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
             </div>
         </nav>
     )
