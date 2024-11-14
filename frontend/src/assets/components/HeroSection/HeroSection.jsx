f7f731bacc01e1c3b5261c4746730b361626da0e
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-11-14 23:23:09 +0530

    Hero Section and New Arrivals Section

--- frontend/src/assets/components/HeroSection/HeroSection.jsx
+++ frontend/src/assets/components/HeroSection/HeroSection.jsx
@@ -6,18 +6,21 @@ const HeroSection = () => {
         // <div className='relative flex items-center bg-cover flex-start bg-center h-screen w-full' style={{backgroundImage: `url(${HeroImg})`}}>
 
         // </div>
-        <div className='relative flex items-center flex-start h-fit w-full bg-gradient-to-t from-[#1c7a83] via-[#25a4af] to-[#2ec7d4]'>
-            <div className='w-full h-full p-4 text-center'>
-                <div className='text-6xl text-white'>
+        <div className='relative flex items-center h-fit w-full bg-gradient-to-t from-[#1c7a83] via-[#25a4af] to-[#2ec7d4]'>
+            <div className='w-3/5 min-w-[500px] h-full p-4 text-center'>
+                <div className='text-4xl lg:text-5xl xl:text-6xl text-white min-w-96'>
                     Find Your Perfect Look, <br />
                     Every Time
                 </div>
 
-                <button className='bg-white rounded-md px-10 py-3 shadow-md mt-16 border border-gray-400'>Shop Now</button>
-                
+                <button className='bg-white rounded-md px-10 py-3 shadow-md mt-16 hover:bg-black hover:text-white'>Shop Now</button>             
                 
             </div>
-            <img src={HeroImg} alt="" className='w-1/3 mt-10 mr-10' />
+
+            <div className='w-2/5 flex justify-center'>
+                <img src={HeroImg} alt="" className='w-full min-w-[300px] mt-10 mr-10' />
+            </div>
+            
         </div>
     )
 }
---
+++ frontend/src/assets/components/Sections/SectionHeading/SectionHeading.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import propTypes from 'prop-types'
+
+const SectionHeading = ({title}) => {
+    return (
+        <div className='flex flex-wrap px-10 my-5 items-center gap-4'>
+            <div className='border rounded-full bg-black w-2 h-16'></div>
+            <p className='text-3xl'>{title}</p>
+        </div>
+    )
+}
+
+SectionHeading.defaultProps = {
+
+}
+
+SectionHeading.propTypes = {
+    title:String
+}
+
+export default SectionHeading
