84dff3eae1b4eea22d91e3d05bfa16d96f5c3f90
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-12-04 23:53:17 +0530

    Product Details page updated

--- frontend/src/assets/pages/ProductDetailPage/ProductDetails.jsx
+++ frontend/src/assets/pages/ProductDetailPage/ProductDetails.jsx
@@ -5,8 +5,32 @@ import content from '../../../data/content.json'
 import Rating from '../../components/Rating/Rating';
 import SizeFilter from '../../components/Filters/SizeFilter'
 import ProductColors from './ProductColors';
+import { IoCartOutline, IoShirtOutline } from 'react-icons/io5';
+import { CiCreditCard1 } from 'react-icons/ci';
+import { BsTruck } from 'react-icons/bs';
+import { TbExchange } from 'react-icons/tb';
+import SectionHeading from '../../components/Sections/SectionHeading/SectionHeading'
 
 const categories = content?.filter_categories;
+
+const extraSections = [
+    {
+        icon: <CiCreditCard1 className='text-2xl text-gray-600' />,
+        label: 'Secure Payment'
+    },
+    {
+        icon: <IoShirtOutline className='text-2xl text-gray-500' />,
+        label: 'Size & Fit'
+    },
+    {
+        icon: <BsTruck className='text-2xl text-gray-500' />,
+        label: 'Free Shipping'
+    },
+    {
+        icon: <TbExchange className='text-2xl text-gray-500' />,
+        label: 'Free Shipping & Returns'
+    }
+];
 
 const ProductDetails = () => {
     const { product } = useLoaderData();
@@ -38,58 +62,92 @@ const ProductDetails = () => {
     }, [productCategory, product])
 
     return (
-        <div className='flex flex-col md:flex-row p-10 gap-10'>
-            <div className='w-[100%] lg:w-[50%] md:w-[40%]'>
-                {/* Image */}
-                <div className='flex flex-col md:flex-row gap-8'>
-                    <div className='w-[100%] md:w-[20%] gap-20'>
-                        <div className='h-full flex flex-row md:flex-col justify-center items-center gap-5'>
-                            {
-                                product?.images?.map((item, index) => (
-                                    <button 
-                                    key={index}
-                                        className='p-1 rounded-md shadow-md border border-gray-200 hover:scale-105'
-                                        onClick={() => setImage(item)}
-                                    >
-                                        <img src={'../../'+item} alt={'sample-'+index} className='w-[64px] h-[64px] object-cover rounded-md' />
-                                    </button>
-                                ))
-                            }
+        <div>
+            <div className='flex flex-col md:flex-row p-10 gap-10 lg:gap-20 justify-center'>
+                <div className='w-[100%] lg:w-[50%] md:w-[80%]'>
+                    {/* Image */}
+                    <div className='flex flex-col md:flex-row gap-10 lg:gap-0'>
+                        <div className='w-[100%] md:w-[20%] gap-20'>
+                            <div className='h-full flex flex-row md:flex-col justify-center items-center gap-5'>
+                                {
+                                    product?.images?.map((item, index) => (
+                                        <button 
+                                        key={index}
+                                            className='p-1 rounded-md shadow-md border border-gray-200 hover:scale-105'
+                                            onClick={() => setImage(item)}
+                                        >
+                                            <img src={'../../'+item} alt={'sample-'+index} className='w-[64px] h-[64px] object-cover rounded-md' />
+                                        </button>
+                                    ))
+                                }
+                            </div>
+                        </div>
+                        <div className='lg:w-[80%] w-[100%] mx-auto'>
+                            <img src={`../../` + image} alt={product?.title} className='h-full w-full max-h-[700px] object-cover bg-center border rounded shadow-md' />
                         </div>
                     </div>
-                    <div className='w-[80%] mx-auto'>
-                        <img src={`../../` + image} alt={product?.title} className='h-full w-full max-h-[700px] object-cover bg-center border rounded shadow-md' />
+                </div>
+                <div className='w-[100%] lg:w-[50%] py-5'>
+                    {/* Product Description */}
+                    <Breadcrumb links={breadcrumbLinks} />
+
+                    <div className='mt-5'>
+                        <p className='text-3xl mb-3'>{product?.title}</p>
+                        <Rating rating={product?.rating} />                    
+                    </div>
+
+                    {/* Price Tag */}
+                    <div className='mt-5 text-2xl font-bold'>Rs. {product?.price}</div>
+                    
+                    <div className='flex flex-col mt-5'>
+                        <div className='flex gap-5'>
+                            <p className='font-bold'>Select Size</p>
+                            <Link 
+                                className='text-gray-500 hover:text-gray-900'
+                                to={'https://en.wikipedia.org/wiki/Clothing_sizes'}
+                            >
+                                {'Size Guide ->'}
+                            </Link>
+                        </div>
+                        <SizeFilter sizes={product?.size} hideTitle />
+                    </div>
+
+                    <div className='mt-2'>
+                        <p className='font-bold'>Colours Available</p>
+                        <ProductColors colors={product?.color} />
+                    </div>
+
+                    <div className='flex mt-10'>
+                        <button className='flex items-center justify-center gap-4 px-8 py-3 bg-black text-white rounded-md shadow-md'>
+                            <IoCartOutline className='text-2xl' />
+                            <div className='font-light'>Add to Cart</div>
+                        </button>
+                    </div>
+
+                    <div className='w-full mt-8 grid lg:grid-cols-2 grid-cols-1 gap-4'>
+                        {
+                            extraSections?.map((section) => (
+                                <div className='flex gap-4 justify-center items-center w-fit'>
+                                    <div className='bg-gray-100 p-2 rounded-full shadow-md border'>
+                                        {section?.icon}
+                                    </div>
+                                    <p>{section.label}</p>
+                                </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
-            <div className='w-[60%]'>
-                {/* Product Description */}
-                <Breadcrumb links={breadcrumbLinks} />
 
-                <div className='mt-5'>
-                    <p className='text-3xl mb-3'>{product?.title}</p>
-                    <Rating rating={product?.rating} />                    
-                </div>
-                
-                <div className='flex flex-col mt-5'>
-                    <div className='flex gap-5'>
-                        <p className='font-bold'>Select Size</p>
-                        <Link 
-                            className='text-gray-500 hover:text-gray-900'
-                            to={'https://en.wikipedia.org/wiki/Clothing_sizes'}
-                        >
-                            {'Size Guide ->'}
-                        </Link>
-                    </div>
-                    <SizeFilter sizes={product?.size} hideTitle />
-                </div>
-
-                <div className='mt-2'>
-                    <p className='font-bold'>Colours Available</p>
-                    <ProductColors colors={product?.color} />
+            {/* Product Description */}
+            <div className='px-10 pt-5 pb-10 md:w-[50%] w-full'>
+                <div>
+                    <SectionHeading title={'Product Description'} />
+                    <p className='mx-10'>{product?.description}</p>
                 </div>
             </div>
         </div>
+        
     )
 }
 
