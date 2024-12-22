38977bea86533b41deb596c8cff567ca65d5bd1d
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2024-12-22 23:42:45 +0530

    REST API Integration

--- frontend/src/assets/pages/Wrapper.jsx
+++ frontend/src/assets/pages/Wrapper.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import NavigationBar from '../components/Navigation/NavigationBar'
 import { Outlet } from 'react-router-dom'
+import Spinner from '../components/Spinner/Spinner'
 
 const Wrapper = () => {
   return (
     <div>
         <NavigationBar />
         <Outlet />
+        <Spinner />
     </div>
   )
 }
