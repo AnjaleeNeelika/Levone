08833bc8fa814daf9efc5ef67838fd8a7120df08
AnjaleeNeelika <108944238+AnjaleeNeelika@users.noreply.github.com>
2025-03-04 23:51:03 +0530

    Signup page design and API integration

--- frontend/src/api/authentication.jsx
+++ frontend/src/api/authentication.jsx
@@ -20,19 +20,21 @@ export const loginAPI = async(body) => {
         return response?.data;
     } catch (err) {
         throw new Error(err);
-        
-        // if (err.response) {
-        //     // Log more details for debugging
-        //     console.error("Error response data:", err.response.data);
-        //     console.error("Error response status:", err.response.status);
-        //     console.error("Error response headers:", err.response.headers);
-        //     throw new Error(err.response.data.message || "Something went wrong!");
-        // } else if (err.request) {
-        //     console.error("No response received:", err.request);
-        //     throw new Error("No response from server.");
-        // } else {
-        //     console.error("Error:", err.message);
-        //     throw new Error("An error occurred while making the request.");
-        // }
+    }
+}
+
+export const registerAPI = async (body) => {
+    const url = API_BASE_URL + '/api/auth/register';
+
+    try {
+        const response = await axios(url, {
+            method: 'POST',
+            data: body
+        });
+
+        return response?.data;
+    }
+    catch(err) {
+        throw new Error(err);
     }
 }
--- frontend/src/assets/pages/Login/Login.jsx
+++ frontend/src/assets/pages/Login/Login.jsx
@@ -53,14 +53,14 @@ const Login = () => {
                 <img src={BackgroundImg} alt="shopping_img" className='w-[50%] lg:w-[60%] hidden md:inline rounded-xl object-cover' />
             {/* </div> */}
             <div className='lg:w-[40%] w-full flex items-center justify-center'>
-                <div className='w-full max-w-[320px] md:w-[320px] py-4 px-6 mx-auto'>
+                <div className='w-full max-w-[320px] lg:w-[320px] py-4 px-6 mx-auto'>
                     <p className='w-fit font-bold text-3xl mb-5'>Sign In</p>
                     <GoogleSignIn />
                     <p className='text-gray-500 items-center text-center w-full py-5'>OR</p>
 
                     <div>
                         <form onSubmit={onSubmit}>
-                            <input type="email" name='userName' value={values?.userName} onChange={handleOnChange} placeholder='Email Address' className='w-full py-2 px-4 border border-gray-400 rounded-md text-gray-600 hover:border-black' required />
+                            <input type="email" name='userName' value={values?.userName} onChange={handleOnChange} placeholder='Email Address' className='w-full py-2 px-4 border border-gray-400 rounded-md text-gray-600 hover:border-black' required autoComplete />
                             <input type="password" name='password' value={values?.password} onChange={handleOnChange} placeholder='Password' className='w-full py-2 px-4 mt-5 border border-gray-400 rounded-md text-gray-600 hover:border-black' required autoComplete='new-password' />
                             <Link className='text-right w-full float-right underline pt-2 text-sm text-gray-500 hover:text-black'>Forgot Password?</Link>
                             <button className='border w-full rounded-lg p-3 bg-black text-white shadow-lg mt-10 hover:opacity-80 hover:-translate-y-1 transition-transform duration-200'>Sign In</button>
@@ -68,7 +68,9 @@ const Login = () => {
                     </div>
                     {error && <p className='text-lg text-red-700'>{error}</p>}
                     <div className='mt-2'>
-                        <Link className='mt-5 text-sm text-gray-500 underline hover:text-black'>Don't have an account? Sign up</Link>
+                        <Link 
+                            to={'/v1/signup'}
+                            className='mt-5 text-sm text-gray-500 underline hover:text-black'>Don't have an account? Sign Up</Link>
                     </div>
                 </div>
             </div>
